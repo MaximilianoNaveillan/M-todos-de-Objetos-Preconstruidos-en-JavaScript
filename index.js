9d9c7b25d8255ad79c8cb6583cb123f133508d10
elfercape <elfercape@gmail.com>
2026-02-09 21:45:10 -0300

    agregando metodos de objetos 09-02-2026 21:45

---
+++ index.js
@@ -0,0 +1,19 @@
+//vamos a retornar Math.abs//
+//Math retorna un valor absoluto//
+console.log(Math.abs(-4.7));
+console.log(Math.abs('-100'));
+// Math.round devuelve el numero redondeado al entero mas cercano//
+console.log(Math.round(4.9));
+console.log(Math.round(4.5));
+console.log(Math.round(4.4999999));
+console.log(Math.round('4.9'));
+console.log(Math.round(1 / 3));
+console.log(Math.round(-1.4));
+console.log(Math.abs(Math.round(-1.4)));
+console.log(Math.abs(Math.round('uno')));
+//Math.ceil(x)devuelve el valor de x redondeado a su entero más cercano hacia arriba://
+console.log(Math.ceil(4.1));
+console.log(Math.ceil(-4.1));
+//Math.floor(x)devuelve el valor de x redondeado hacia abajo a su entero más cercano://
+console.log(Math.floor(-4.1));
+console.log(Math.floor(5.1));
